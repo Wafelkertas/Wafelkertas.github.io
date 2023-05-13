d6802fa40e274742a882c22bd2e938df0954a553
Shidqi Akram Hauzan <57066616+Wafelkertas@users.noreply.github.com>
2023-05-13 13:49:12 +0700

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,79 @@
+import './global.css';
+import clsx from 'clsx';
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import Sidebar from '../components/sidebar';
+import { Analytics } from '@vercel/analytics/react';
+
+const kaisei = localFont({
+  src: '../public/fonts/kaisei-tokumin-latin-700-normal.woff2',
+  weight: '700',
+  variable: '--font-kaisei',
+  display: 'swap',
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: 'WafelKertas',
+    template: '%s | Shidqi Akram Hauzan',
+  },
+  description: 'Developer and creator.',
+  openGraph: {
+    title: 'Shidqi Akram Hauzan',
+    description: 'Developer and creator.',
+    url: 'https://wafelkertas.io',
+    siteName: 'WafelKertas',
+    images: [
+      {
+        url: 'https://user-images.githubusercontent.com/57066616/237603619-17dcd451-bd59-4a7e-9b8c-bd2ff429d140.jpg',
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: 'en-US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  
+  icons: {
+    shortcut: '/favicon.ico',
+  },
+  verification: {
+    google: 'eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw',
+    yandex: '14d2e73487fa6c71',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={clsx(
+        'text-black bg-white dark:text-white dark:bg-[#111010]',
+        kaisei.variable
+      )}
+    >
+      <body className="antialiased max-w-4xl mb-40 flex flex-col md:flex-row mx-4 mt-8 md:mt-20 lg:mt-32 lg:mx-auto">
+        <Sidebar />
+        <main className="flex-auto min-w-0 mt-6 md:mt-0 flex flex-col px-2 md:px-0">
+          {children}
+          <Analytics />
+        </main>
+      </body>
+    </html>
+  );
+}
